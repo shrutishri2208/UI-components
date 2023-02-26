ca4d0df48c0c77b03d0bcfb5499e1d36240c1dd3
Shruti Shrivastava <shrutishri2208@gmail.com>
2023-02-26 21:40:59 +0530

    responsive2

--- src/components/Submit2.js
+++ src/components/Submit2.js
@@ -8,7 +8,7 @@ const Submit2 = () => {
   return (
     <div className="border-2 rounded-lg p-8 w-full">
       <h1 className="mb-4">Loader on Click - 2</h1>
-      <div className="grid grid-cols-2 sub-2 ">
+      <div className="grid 2xl:grid-cols-1 xl:grid-cols-1 lg:grid-cols-1 md:grid-cols-2 sub-2 ">
         <Button5 loaderTime={loaderTime} />
         <Button6 loaderTime={loaderTime} />
       </div>
